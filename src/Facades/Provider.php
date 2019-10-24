dc0e682724d915a2875a16e79f8813ac876477b7
Ronan Chilvers <ronan@thelittledot.com>
2019-10-24 21:56:11 +0100

    Add method hint

--- src/Facades/Provider.php
+++ src/Facades/Provider.php
@@ -9,6 +9,7 @@ use Ronanchilvers\Foundation\Facade\Facade;
  * Provider factory facade class
  *
  * @method static \App\Provider\ProviderInterface forProject(\App\Model\Project $project)
+ * @method static array getOptions()
  * @author Ronan Chilvers <ronan@d3r.com>
  */
 class Provider extends Facade
